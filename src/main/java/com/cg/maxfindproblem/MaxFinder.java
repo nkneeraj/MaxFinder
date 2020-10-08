7a67f3afa6c11a416aa7624cca5cfdccd318a704
nkneeraj <nkneerj641@gmail.com>
2020-10-08 23:00:33 +0530

    [NK]Refactored: All 3 max method to one generic method.

--- src/main/java/com/cg/maxfindproblem/MaxFinder.java
+++ src/main/java/com/cg/maxfindproblem/MaxFinder.java
@@ -2,34 +2,40 @@ package com.cg.maxfindproblem;
 
 public class MaxFinder {
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to MaxFinder");
+	public <E extends Comparable<E>> E find_maximum(E a, E b, E c)
+		{
+			E max = a;
+			if(b.compareTo(max)>0)
+				max=b;
+			if(c.compareTo(max)>0)
+				max=c;
+			return max;
 	}
 
-	public Integer find_maximum(Integer a, Integer b, Integer c) {
-		Integer max = a;
-		if (b.compareTo(max) > 0)
-			max = b;
-		if (c.compareTo(max) > 0)
-			max = c;
-		return max;
-	}
-
-	public Float find_maximum(Float a, Float b, Float c) {
-		Float max = a;
-		if (b.compareTo(max) > 0)
-			max = b;
-		if (c.compareTo(max) > 0)
-			max = c;
-		return max;
-	}
-
-	public String find_maximum(String a, String b, String c) {
-		String max = new String(a);
-		if (b.compareTo(max) > 0)
-			max = new String(b);
-		if (c.compareTo(max) > 0)
-			max = new String(c);
-		return max;
-	}
+//	public Integer find_maximum(Integer a, Integer b, Integer c) {
+//		Integer max = a;
+//		if (b.compareTo(max) > 0)
+//			max = b;
+//		if (c.compareTo(max) > 0)
+//			max = c;
+//		return max;
+//	}
+//
+//	public Float find_maximum(Float a, Float b, Float c) {
+//		Float max = a;
+//		if (b.compareTo(max) > 0)
+//			max = b;
+//		if (c.compareTo(max) > 0)
+//			max = c;
+//		return max;
+//	}
+//
+//	public String find_maximum(String a, String b, String c) {
+//		String max = new String(a);
+//		if (b.compareTo(max) > 0)
+//			max = new String(b);
+//		if (c.compareTo(max) > 0)
+//			max = new String(c);
+//		return max;
+//	}
 }
