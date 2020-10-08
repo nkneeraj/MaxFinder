fb2cbae3f23613893c394d4f23042ddc88244a20
nkneeraj <nkneerj641@gmail.com>
2020-10-08 17:58:48 +0530

    [NK]Added Uc1: Max of 3 numbers.

--- src/main/java/com/cg/maxfindproblem/MaxFinder.java
+++ src/main/java/com/cg/maxfindproblem/MaxFinder.java
@@ -6,12 +6,12 @@ public class MaxFinder {
 		System.out.println("Welcome to MaxFinder");
 	}
 
-//	public Integer find_maximum(Integer a, Integer b, Integer c) {
-//		Integer max = a;
-//		if (b.compareTo(max) > 0)
-//			max = b;
-//		if (c.compareTo(max) > 0)
-//			max = c;
-//		return max;
-//	}
+	public Integer find_maximum(Integer a, Integer b, Integer c) {
+		Integer max = a;
+		if (b.compareTo(max) > 0)
+			max = b;
+		if (c.compareTo(max) > 0)
+			max = c;
+		return max;
+	}
 }
--- src/test/resources/maxfindproblem/MaxFinderTest.java
+++ src/test/resources/maxfindproblem/MaxFinderTest.java
@@ -2,33 +2,26 @@ package maxfindproblem;
 
 import static org.junit.Assert.assertEquals;
 
-import org.junit.Before;
 import org.junit.Test;
 
 import com.cg.maxfindproblem.MaxFinder;
 
 public class MaxFinderTest {
-//	MaxFinder maxfinder;
-//	@Before
-//	public void before() {
-//		maxfinder=new MaxFinder();
-//	}
 
 	@Test
 	public void test1_1() {
 		MaxFinder maxfinder=new MaxFinder();
-		assertEquals((Integer)9,(Integer)maxfinder.find_maximum(9, 2, 4));
+		assertEquals((Integer)3,(Integer)maxfinder.find_maximum(3, 2, 1));
 	}
 	
 	@Test
 	public void test1_2() {
 		MaxFinder maxfinder=new MaxFinder();
-		assertEquals((Integer)20,(Integer)maxfinder.find_maximum(9, 20, 4));
+		assertEquals((Integer)3,(Integer)maxfinder.find_maximum(2, 3, 1));
 	}
 	@Test
 	public void test1_3() {
 		MaxFinder maxfinder=new MaxFinder();
-		assertEquals((Integer)30,(Integer)maxfinder.find_maximum(9, 2, 30));
+		assertEquals((Integer)3,(Integer)maxfinder.find_maximum(1, 2, 3));
 	}
-
 }
