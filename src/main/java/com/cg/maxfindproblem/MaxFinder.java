8e2cb35fd40717955e113052fe58871882f1622d
nkneeraj <nkneerj641@gmail.com>
2020-10-11 22:08:04 +0530

    [NK]Added UC4: Use of collection and sort fn.

--- src/main/java/com/cg/maxfindproblem/MaxFinder.java
+++ src/main/java/com/cg/maxfindproblem/MaxFinder.java
@@ -1,27 +1,21 @@
 package com.cg.maxfindproblem;
 
+import java.util.ArrayList;
+
 public class MaxFinder<E extends Comparable<E>> {
-	E X;
-	E Y;
-	E Z;
+	ArrayList<E> arr = new ArrayList<>();
 
-	MaxFinder(E value1, E value2, E value3) {
-		this.X = value1;
-		this.Y = value2;
-		this.Z = value3;
-	}
-
-	public static <E extends Comparable<E>> E find_maximum(E a, E b, E c) {
-		E max = a;
-		if (b.compareTo(max) > 0)
-			max = b;
-		if (c.compareTo(max) > 0)
-			max = c;
-		return max;
+	public ArrayList<E> add(E a, E b, E c , E d)
+	{
+		arr.add(a);
+		arr.add(b);
+		arr.add(c);
+		arr.add(d);
+		return arr;
 	}
 	
-	public <E extends Comparable<E>> E find_maximum() {
-		return (E) find_maximum(X, Y, Z);
+	public <E extends Comparable<E>> E find_maximum(ArrayList<E> arr) {
+		return arr.get(arr.size()-1);
 	}
 	
 	public static void main(String[] args) {
